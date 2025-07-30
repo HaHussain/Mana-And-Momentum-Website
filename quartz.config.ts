5fdeea9f9926b34da734f4c931f7920ace62c466
HH <HaHussain010@gmail.com>
2025-07-30 13:29:56 +0100

    BREAKING: change from shortest link resolution to Absolute

--- quartz.config.ts
+++ quartz.config.ts
@@ -68,7 +68,7 @@ const config: QuartzConfig = {
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
       Plugin.HardLineBreaks(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.CrawlLinks({ markdownLinkResolution: "absolute" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
       Plugin.wikilinkIconer(),
